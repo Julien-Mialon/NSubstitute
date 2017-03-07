7acdefa4d36ac1f0cc65ec8be32dab2be3bc5067
Julien Mialon <mialon.julien@gmail.com>
2017-03-07 14:05:45 +0100

    Fix RobusThreadLocal implementation for DotNetCore

--- Source/NSubstitute/Core/RobustThreadLocal.cs
+++ Source/NSubstitute/Core/RobustThreadLocal.cs
@@ -3,13 +3,14 @@ using System.Threading;
 
 namespace NSubstitute.Core
 {
-    /// <summary>
-    /// Delegates to ThreadLocal&lt;T&gt;, but wraps Value property access in try/catch to swallow ObjectDisposedExceptions.
-    /// These can occur if the Value property is accessed from the finalizer thread. Because we can't detect this, we'll
-    /// just swallow the exception (the finalizer thread won't be using any of the values from thread local storage anyway).
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    class RobustThreadLocal<T>
+#if NET45
+	/// <summary>
+	/// Delegates to ThreadLocal&lt;T&gt;, but wraps Value property access in try/catch to swallow ObjectDisposedExceptions.
+	/// These can occur if the Value property is accessed from the finalizer thread. Because we can't detect this, we'll
+	/// just swallow the exception (the finalizer thread won't be using any of the values from thread local storage anyway).
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	class RobustThreadLocal<T>
     {
         readonly ThreadLocal<T> _threadLocal;
         public RobustThreadLocal() { _threadLocal = new ThreadLocal<T>(); }
@@ -28,4 +29,52 @@ namespace NSubstitute.Core
             }
         }
     }
+#else
+	using System.Collections.Concurrent;
+	class RobustThreadLocal<T>
+	{
+		readonly ConcurrentDictionary<string, T> contextMappings = new ConcurrentDictionary<string, T>();
+		readonly Func<T> initialValue;
+		readonly AsyncLocal<string> mappingKeyAsyncLocal;
+
+		public RobustThreadLocal()
+			: this(() => default(T)) { }
+
+		public RobustThreadLocal(Func<T> initialValue)
+		{
+			this.initialValue = initialValue;
+
+			mappingKeyAsyncLocal = new AsyncLocal<string>
+			{
+				Value = null
+			};
+		}
+
+		string GetMappingKey()
+		{
+			var mappingKey = mappingKeyAsyncLocal.Value;
+			if (mappingKey == null)
+			{
+				mappingKey = Guid.NewGuid().ToString();
+				mappingKeyAsyncLocal.Value = mappingKey;
+			}
+
+			return mappingKey;
+		}
+
+		public T Value
+		{
+			get
+			{
+				var mappingKey = GetMappingKey();
+				return contextMappings.GetOrAdd(mappingKey, _ => initialValue());
+			}
+			set
+			{
+				var mappingKey = GetMappingKey();
+				contextMappings[mappingKey] = value;
+			}
+		}
+	}
+#endif
 }
